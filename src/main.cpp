8855c8f06feea4ceb3a04617d3cae3239cef3900
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-14 09:17:48 -0400

    android... sheesh

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ const std::filesystem::path resourcesPath = (std::filesystem::current_path() / "
 using namespace geode::prelude;
 
 class $modify(MyFMODAudioEngine, FMODAudioEngine) {
+	#ifdef GEODE_IS_DESKTOP
 	void playEffectAdvanced(gd::string p0, float p1, float p2, float p3, float p4, bool p5, bool p6, int p7, int p8, int p9, int p10, bool p11, int p12, bool p13, bool p14, int p15, int p16, float p17, int p18) {
 		if (Mod::get()->getSettingValue<bool>("enabled") && !std::regex_match(std::string(p0), mp3Regex)) {
 			if (std::find(vanillaSFX.begin(), vanillaSFX.end(), std::string(p0)) == vanillaSFX.end()) {
@@ -17,6 +18,7 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 			} else {
 				auto volume = (Mod::get()->getSettingValue<int64_t>("volume") / 100.0f);
 				if (Mod::get()->getSettingValue<double>("volumeBoost") != 1.0) { volume *= Mod::get()->getSettingValue<double>("volumeBoost"); }
+				if (volume < 0.001f) { volume = 0.0001f; }
 
 				auto system = FMODAudioEngine::sharedEngine()->m_system;
 
@@ -43,4 +45,39 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 			FMODAudioEngine::playEffectAdvanced(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
 		}
 	}
+	#else
+	void playEffect(gd::string p0, float p1, float p2, float p3) {
+		if (Mod::get()->getSettingValue<bool>("enabled") && !std::regex_match(std::string(p0), mp3Regex)) {
+			if (std::find(vanillaSFX.begin(), vanillaSFX.end(), std::string(p0)) == vanillaSFX.end()) {
+				FMODAudioEngine::playEffect(p0, p1, p2, p3);
+			} else {
+				auto volume = (Mod::get()->getSettingValue<int64_t>("volume") / 100.0f);
+				if (Mod::get()->getSettingValue<double>("volumeBoost") != 1.0) { volume *= Mod::get()->getSettingValue<double>("volumeBoost"); }
+
+				auto system = FMODAudioEngine::sharedEngine()->m_system;
+
+				FMOD::Channel* channel;
+				FMOD::Sound* sound;
+
+				std::filesystem::path sfxPath = (resourcesPath / p0.c_str());
+
+				for (auto& p : CCFileUtils::sharedFileUtils()->getSearchPaths()) {
+					auto path = std::filesystem::path(p.c_str()).parent_path();
+					if (!strcmp(path.parent_path().filename().string().c_str(), "packs") || !strcmp(path.parent_path().filename().string().c_str(), "unzipped")) {
+						if (std::filesystem::exists(path / std::string(p0))) {
+							sfxPath = path / std::string(p0);
+							break;
+						}
+					}
+				}
+
+				system->createSound(sfxPath.string().c_str(), FMOD_DEFAULT, nullptr, &sound);
+				system->playSound(sound, nullptr, false, &channel);
+				channel->setVolume(volume);
+			}
+		} else {
+			FMODAudioEngine::playEffect(p0, p1, p2, p3);
+		}
+	}
+	#endif
 };
